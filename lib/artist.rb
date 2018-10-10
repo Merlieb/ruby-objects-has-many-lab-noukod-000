10a07565991740c6195eb194d80c5ef00149e760
Merline Belizaire <merline@noukod.com>
2018-10-10 15:03:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,13 @@
 class Artist
+  @@all = []
 attr_accessor :name ,:songs
 def initialize (name)
   @name = name
-end
+  @@all << self
+  end
+  def self.all
+    @@all
+  end
   def add_song_by_name(name, genre)
    song = Song.new(name, genre)
    @songs << song
