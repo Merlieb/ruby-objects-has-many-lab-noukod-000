5a46501a7160b778b053a1f6dc5dc4e225c97e78
Merline Belizaire <merline@noukod.com>
2018-10-10 15:10:48 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,24 @@
 class Artist
-  @@all = []
-attr_accessor :name
-def initialize (name)
-  @name = name
-  @@all << self
+
+  attr_accessor :name
+    @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
   end
   def self.all
     @@all
-  end
-  def add_song_by_name(name)
-   song = Song.new(name)
-   @songs << song
-    song.artist = self
   end
 
   def add_song(song)
     song.artist = self
   end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
   def songs
     Song.all.select {|song| song.artist == self}
   end
