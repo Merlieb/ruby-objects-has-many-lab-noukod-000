7fd5bbc29833e71753fdef3f53e6795599baba9d
Merline Belizaire <merline@noukod.com>
2018-10-10 15:08:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,8 +8,8 @@ def initialize (name)
   def self.all
     @@all
   end
-  def add_song_by_name(name, genre)
-   song = Song.new(name, genre)
+  def add_song_by_name(name)
+   song = Song.new(name)
    @songs << song
     song.artist = self
   end
